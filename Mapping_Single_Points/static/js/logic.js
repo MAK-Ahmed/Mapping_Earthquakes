0da43b79f01b876c348cd8f1986cd19cd6a3d7ab
MAK-Ahmed <MAhmed27@northwell.edu>
2022-11-23 13:36:01 -0500

    update files

---
+++ Mapping_Single_Points/static/js/logic.js
@@ -0,0 +1,6 @@
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+     L.circle([34.0522, -118.2437], {
+         radius: 100
+      }).addTo(map);
